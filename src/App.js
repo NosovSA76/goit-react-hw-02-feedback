892f7cbd2e0ecd1ffd43d12d17d4b0d49c464548
NosovSA76 <125302593+NosovSA76@users.noreply.github.com>
2023-07-10 08:35:39 +0300

    after review

--- src/App.js
+++ src/App.js
@@ -1,28 +1,32 @@
 import React, { Component } from "react";
-import { Buttons } from "./components/buttons/buttons";
-import { TitleFeedback } from "./components/title/title";
-import { StatisticTitle } from "./components/statisticTitle/statistikTitle";
-import { StatisticField } from "./components/statistics/statistics";
-import { handleButtonClick, getBackgroundColor } from "./utils";
-import { Section } from "./components/feedbackboard/feedbackboard";
+import { Buttons } from "./components/Buttons/buttons";
+import { TitleFeedback } from "./components/Title/title";
+import { StatisticTitle } from "./components/StatisticTitle/statistikTitle";
+import { StatisticField } from "./components/Statistics/statistics";
+import { getBackgroundColor } from "./utils/bacgrond";
+import { Section } from "./components/Feedbackboard/feedbackboard";
 
 export class App extends Component {
   state = {
     good: 0,
     neutral: 0,
     bad: 0,
-    totalScore: 0,
-    percentage: 0,
-    goodPercentage: 0,
   };
 
   handleButtonClick = (buttonType) => {
-    handleButtonClick.call(this, buttonType);
+    this.setState((prevState) => {
+      return {
+        [buttonType]: prevState[buttonType] + 1,
+      };
+    });
   };
 
   render() {
-    const { good, neutral, bad, percentage, goodPercentage } = this.state;
-    const total = good + neutral + bad;
+    const { good, neutral, bad } = this.state;
+    let total = good + neutral + bad;
+    let totalScore = good * 1 + neutral * 0.5 + bad * 0;
+    let goodPercentage = Math.round((good / total) * 100);
+    let percentage = Math.round((totalScore / total) * 100);
     const backgroundColor = getBackgroundColor(percentage, total);
 
     return (
--- src/utils/index.js
+++
@@ -1,2 +0,0 @@
-export * from "./bacgrond";
-export * from "./counter";
